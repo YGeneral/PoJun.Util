91aebf47666a22fa6e9f202865cf3b0855545c81
杨江军 <jiangjun.yang@ashermed.com>
2020-08-31 15:16:31 +0800

    update config

--- source/netstandard2.0/PoJun.Util/Helpers/Downloader/PartialDownloader.cs
+++ source/netstandard2.0/PoJun.Util/Helpers/Downloader/PartialDownloader.cs
@@ -166,7 +166,7 @@ namespace PoJun.Util.Helpers
                     req.ServicePoint.Expect100Continue = true;
                     req.ProtocolVersion = HttpVersion.Version11;
                     config(req);
-                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12 | SecurityProtocolType.Ssl3;
+                    ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
                     ServicePointManager.Expect100Continue = true;
                     if (RangeAllowed)
                     {
