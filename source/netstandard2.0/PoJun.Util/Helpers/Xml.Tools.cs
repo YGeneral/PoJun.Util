d092c34cd817bf4d3580693cb91f72f7c1d582f6
杨江军 <jiangjun.yang@ashermed.com>
2021-03-04 17:51:14 +0800

    增加XML序列号和反序列化功能

--- source/netstandard2.0/PoJun.Util/Helpers/Xml.Tools.cs
+++ source/netstandard2.0/PoJun.Util/Helpers/Xml.Tools.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Xml.Linq;
 using System.Linq;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace PoJun.Util.Helpers
 {
@@ -30,6 +32,98 @@ namespace PoJun.Util.Helpers
             if (document?.Root == null)
                 return new List<XElement>();
             return document.Root.Elements().ToList();
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <param name="encoding">目前只支持utf8、ASCII、gb2312，且默认utf8</param>
+        /// <returns></returns>
+        public static string Serialize<T>(T obj, string encoding = "utf8")
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            StringWriter writer;
+            switch(encoding.ToLower())
+            {                
+                case "ascii":
+                    writer = new StringASCIIWriter();
+                    break;
+                case "gb2312":
+                    writer = new StringGB2312Writer();
+                    break;
+                case "utf8":
+                default:
+                    writer = new StringUTF8Writer();
+                    break;
+            }
+            serializer.Serialize(writer, obj);
+            string xml = writer.ToString();
+            writer.Close();
+            writer.Dispose();
+
+            return xml;
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xml"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string xml)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            StringReader reader = new StringReader(xml);
+            T result = (T)(serializer.Deserialize(reader));
+            reader.Close();
+            reader.Dispose();
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringUTF8Writer : System.IO.StringWriter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringASCIIWriter : System.IO.StringWriter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Encoding Encoding
+        {
+            get { return Encoding.ASCII; }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class StringGB2312Writer : System.IO.StringWriter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public override Encoding Encoding
+        {
+            get { return Encoding.GetEncoding("GB2312"); }
         }
     }
 }
